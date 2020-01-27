1862051afe2a53656e04a4eb191fd5929d192251
Anna Craig <acraig@agari.com>
2020-01-27 13:49:06 -0800

    Adding classification evaluation plots; still need to update comments; TODO bug using log_loss metric

--- plotting_utilities.py
+++ plotting_utilities.py
@@ -3,9 +3,12 @@ from matplotlib.gridspec import GridSpec
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import pickle
 from scipy.stats import gaussian_kde
+import sys
 
-
+from sklearn.calibration import CalibratedClassifierCV
+from sklearn.metrics import confusion_matrix
 
 def feature_distributions(data, features, value_col, ncols = 3, 
                           highlighting = None, edge_values = None,
@@ -238,3 +241,244 @@ def jointplot(x, y, c = 'k', cmap = 'gray_r',
 
 
     return fig, ax_joint, ax_marg_x, ax_marg_y
+
+
+# Adapted from https://matplotlib.org/gallery/images_contours_and_fields/image_annotated_heatmap.html
+def heatmap(data, row_labels, col_labels,
+            row_title = '', col_title = '', ax=None,
+            cbar_kw={}, cbarlabel="", vmin=None, vmax=None,
+            x_tick_rotation = 0, **kwargs):
+    """
+    Create a heatmap from a numpy array and two lists of labels.
+
+    Arguments:
+        data       : A 2D numpy array of shape (N,M)
+        row_labels : A list or array of length N with the labels
+                     for the rows
+        col_labels : A list or array of length M with the labels
+                     for the columns
+    Optional arguments:
+        ax         : A matplotlib.axes.Axes instance to which the heatmap
+                     is plotted. If not provided, use current axes or
+                     create a new one.
+        cbar_kw    : A dictionary with arguments to
+                     :meth:`matplotlib.Figure.colorbar`.
+        cbarlabel  : The label for the colorbar
+    Returns:
+        im, cbar : handles to image and colorbar
+
+    All other arguments are directly passed on to the imshow call.
+    """
+
+    if not ax:
+        ax = plt.gca()
+        
+    if not vmin:
+        vmin = np.amin(data)
+        
+    if not vmax:
+        vmax = np.amax(data)
+
+    # Plot the heatmap
+    im = ax.imshow(data, **kwargs)
+
+    # Create colorbar
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+
+    # We want to show all ticks...
+    ax.set_xticks(np.arange(data.shape[1]))
+    ax.set_yticks(np.arange(data.shape[0]))
+    # ... and label them with the respective list entries.
+    ax.set_xticklabels(col_labels)
+    ax.set_yticklabels(row_labels)
+
+    # Let the horizontal axes labeling appear on top.
+    ax.tick_params(top=False, bottom=True,
+                   labeltop=False, labelbottom=True)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=x_tick_rotation, ha="right",
+             rotation_mode="anchor")
+
+    # Turn spines off and create white grid.
+    for _, spine in ax.spines.items():
+        spine.set_visible(False)
+
+    ax.set_xticks(np.arange(data.shape[1]+1)-.5, minor=True)
+    ax.set_yticks(np.arange(data.shape[0]+1)-.5, minor=True)
+    ax.grid(which="minor", color="k", linestyle='-', linewidth=1)
+    ax.tick_params(which="minor", bottom=True, left=True)
+
+    ax.set_xlabel(col_title)
+    ax.set_ylabel(row_title)
+
+    return im, cbar
+
+# Adapted from https://matplotlib.org/gallery/images_contours_and_fields/image_annotated_heatmap.html
+def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
+                     textcolors=["black", "white"],
+                     threshold=None, **textkw):
+    """
+    A function to annotate a heatmap.
+
+    Arguments:
+        im         : The AxesImage to be labeled.
+    Optional arguments:
+        data       : Data used to annotate. If None, the image's data is used.
+        valfmt     : The format of the annotations inside the heatmap.
+                     This should either use the string format method, e.g.
+                     "$ {x:.2f}", or be a :class:`matplotlib.ticker.Formatter`.
+        textcolors : A list or array of two color specifications. The first is
+                     used for values below a threshold, the second for those
+                     above.
+        threshold  : Value in data units according to which the colors from
+                     textcolors are applied. If None (the default) uses the
+                     middle of the colormap as separation.
+    Returns:
+        texts: list of text annotations
+
+    Further arguments are passed on to the created text labels.
+    """
+
+    if not isinstance(data, (list, np.ndarray)):
+        data = im.get_array()
+
+    # Normalize the threshold to the images color range.
+    if threshold is not None:
+        threshold = im.norm(threshold)
+    else:
+        threshold = im.norm(data.max())/2.
+
+    # Set default alignment to center, but allow it to be
+    # overwritten by textkw.
+    kw = dict(horizontalalignment="center",
+              verticalalignment="center")
+    kw.update(textkw)
+
+    # Get the formatter in case a string is supplied
+    if isinstance(valfmt, str):
+        valfmt = matplotlib.ticker.StrMethodFormatter(valfmt)
+
+    # Loop over the data and create a `Text` for each "pixel".
+    # Change the text's color depending on the data.
+    texts = []
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            kw.update(color=textcolors[im.norm(data[i, j]) > threshold])
+            text = im.axes.text(j, i, valfmt(data[i, j], None), **kw)
+            texts.append(text)
+
+    return texts
+
+def plot_classifier_performance(estimator, metrics_args, f_train, r_train, f_dev, r_dev, 
+                                calibrated = True, thresholds = None):
+    if calibrated:
+        model = CalibratedClassifierCV(estimator, method="sigmoid")
+        title = model.__class__.__name__+ ', '+model.base_estimator.__class__.__name__
+    else:
+        model = estimator
+        title = model.base_estimator.__class__.__name__
+        
+    model.fit(f_train, r_train)
+    mr_dev_probs = model.predict_proba(f_dev)
+    mr_dev_probs = pd.DataFrame(data = mr_dev_probs, 
+                                            columns = model.classes_, 
+                                            index = f_dev.index)
+    
+    if len(model.classes_) == 2:
+        c1 = model.classes_[0]
+        c2 = model.classes_[1]
+        
+        ncols = 2
+        nrows = 1
+        fig, ax = plt.subplots(nrows = nrows, ncols = ncols, sharey = False, sharex = False, 
+                               figsize = (4*ncols,4*nrows))
+        fig.tight_layout(pad=0.4, w_pad=10.0, h_pad=0.0)
+
+        # Find appropriate threshold
+        if thresholds is None:
+            thresholds = np.linspace(0, 1., 100)
+        evaluated_metrics = {}
+        for metric, kwargs in metrics_args:
+            evaluated_metrics[metric.__name__] = []
+        for threshold in thresholds:
+            mr_dev_type = mr_dev_probs.idxmax(axis = 1)
+            mr_dev_type.loc[(mr_dev_probs[c1] >= threshold)] = c1
+                
+            for metric, kwargs in metrics_args:
+                try:
+                    evaluated_metrics[metric.__name__] += [metric(r_dev, mr_dev_type, **kwargs)]
+                except:
+                    evaluated_metrics[metric.__name__] += [np.nan]
+                    continue
+                
+        if 'loss' in metrics_args[0][0].__name__:
+            i = np.argmin(evaluated_metrics[metrics_args[0][0].__name__])
+        else:
+            i = np.argmax(evaluated_metrics[metrics_args[0][0].__name__])
+        
+        # Plot first two metrics
+        m1 = metrics_args[0][0].__name__
+        color = 'r'
+        ax[0].set_xlabel('Threshold')
+        ax[0].set_ylabel(m1, color=color)
+        ax[0].plot(thresholds, evaluated_metrics[m1], color=color, label = m1)
+        ax[0].tick_params(axis='y', labelcolor=color)
+        if len(metrics_args) > 1:
+            ax2 = ax[0].twinx()  # instantiate a second axes that shares the same x-axis
+            m2 = metrics_args[1][0].__name__
+            color = 'b'
+            ax2.set_ylabel(m2, color=color)  # we already handled the x-label with ax1
+            ax2.plot(thresholds, evaluated_metrics[m2], color=color)
+            ax2.tick_params(axis='y', labelcolor=color)
+        plt.axvline(thresholds[i], color = 'k')
+        
+        # Additional model information
+        model_size = float(sys.getsizeof(pickle.dumps(model)))/1.e6
+        title += '\nThreshold: %.2f'%(thresholds[i])+\
+                 '\nModel size: %.2f MB'%(model_size)
+        for k, v in evaluated_metrics.items():
+            title += '\n%s: %.3f'%(k,v[i])
+        fig.suptitle(title, y = 1.4)
+
+    else:
+        ncols = 1
+        nrows = 1
+        fig, ax = plt.subplots(nrows = nrows, ncols = ncols, sharey = False, sharex = False, 
+                               figsize = (8*ncols,8*nrows))
+        fig.tight_layout(pad=0.4, w_pad=0.0, h_pad=0.0)
+        ax = [ax]
+        
+        mr_dev_type = mr_dev_probs.idxmax(axis = 1)
+        evaluated_metrics = {}
+        for metric, kwargs in metrics_args:
+            try:
+                evaluated_metrics[metric.__name__] = metric(r_dev, mr_dev_type, **kwargs)
+            except:
+                continue
+        
+        
+        # Additional model information
+        model_size = float(sys.getsizeof(pickle.dumps(model)))/1.e6
+        title += '\nModel size: %.2f MB'%(model_size)
+        for k, v in evaluated_metrics.items():
+                title += '\n%s: %.3f'%(k,v)
+        fig.suptitle(title, y = 1.1)
+
+
+    # Plot confusion matrix, labeling with other evaluation metrics
+    cm = confusion_matrix(r_dev, mr_dev_type)
+    im, cbar = heatmap(cm, 
+                       row_title ='True class', row_labels=  model.classes_, 
+                       col_title = 'Predicted class', col_labels =  model.classes_,
+                       ax=ax[-1], cmap='gray_r', cbarlabel="Counts")
+    ax[-1].invert_yaxis()
+    ax[-1].invert_xaxis()
+    cbar.remove()
+    texts = annotate_heatmap(im, valfmt="{x:.0f}")
+    normed_cm = cm/float(len(f_dev))*100
+    pct_texts = annotate_heatmap(im, data = normed_cm, valfmt="\n\n({x:.3f}%)")
+
+
+    return mr_dev_type, mr_dev_probs, fig, ax
