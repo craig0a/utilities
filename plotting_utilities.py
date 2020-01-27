8eacf149a14c1f50d55562a3c8235e4adfdd8ded
Anna Craig <acraig@agari.com>
2020-01-27 09:02:24 -0800

    Adding joint plot; still need to update comments

--- plotting_utilities.py
+++ plotting_utilities.py
@@ -1,11 +1,12 @@
 import matplotlib
+from matplotlib.gridspec import GridSpec
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-
-
-
 from scipy.stats import gaussian_kde
+
+
+
 def feature_distributions(data, features, value_col, ncols = 3, 
                           highlighting = None, edge_values = None,
                           xmin = None, xmax = None, n_bins = 100, n_grid = 500, 
@@ -104,3 +105,136 @@ def feature_distributions(data, features, value_col, ncols = 3,
         
     return fig, ax
 
+
+
+def jointplot(x, y, c = 'k', cmap = 'gray_r',
+              xmin = None, xmax = None, xdelta = None,  
+              ymin = None, ymax = None, ydelta = None, 
+              logscale = False, gridsize = 50, bins = None, alpha = 0.2,
+              joint_xlabel = None, joint_ylabel = None,
+              marginal_xlabel = None, marginal_ylabel = None, 
+              fig_axes = None, joint_type = 'hex', scatter_label = '',
+              highlighting = None, edge_values = None, anchor_legend = None):
+    """
+     Joint plot continuous feature distributions. 
+     Option to plot as hexbinned or scatter joint distribution
+     Option to add categorical highlights binning the continuous value (not recommended if plotting histograms) and/or marked edge labels. 
+
+    Arguments:
+        x (pandas series, floats): x-axis continuous values
+        y (pandas series, floats): y-axis continuous values
+        c (color identifier):
+        c_map (colormap identifier):
+        alpha: 
+        xmin (float): minimum value to plot on x axis 
+        xmax (float): maximum value to plot on x axis
+        xdelta
+        ymin (float): minimum value to plot on y axis 
+        ymax (float): maximum value to plot on y axis
+        ydelta
+        bins (int): number of bins for histogram
+        gridsize (int): resolution of hexbin
+        highlighting (list of tuples: (float, float, color, string)): lower bound, upper bound, color, and label for highlighted ranges of continuous variable 
+        edge_values (list of tuples: (float, color, string)): x-location, color, and label for a vertical line marking a specific value of the continuous variable 
+        logscale (bool): set y-scale to be logarithmic
+        anchor_legend (tuple of floats): x,y coordinates to pin legend to axis
+        fig_axes
+
+    Returns:
+        fig, ax_joint, ax_marg_x, ax_marg_y : handles to figure and each of the three subplot axes
+    """
+
+    if fig_axes == None:
+        fig = plt.figure()
+        gs = GridSpec(4,4)
+
+        ax_joint = fig.add_subplot(gs[1:4,0:3])
+        ax_marg_x = fig.add_subplot(gs[0,0:3])
+        ax_marg_y = fig.add_subplot(gs[1:4,3])
+    else:
+        fig,ax_joint,ax_marg_x,ax_marg_y = fig_axes
+       
+    if joint_type == 'hex':
+        ax_joint.hexbin(x,y, cmap = cmap, bins= 'log', gridsize = gridsize )
+    elif joint_type == 'scatter':
+        ax_joint.scatter(x,y, color = c, alpha= alpha, label = scatter_label)
+        
+   
+    if xmin is None:
+        xmin = min(x)
+    if xmax is None:
+        xmax = max(x)
+    if ymin is None:
+        ymin = min(y)
+    if ymax is None:
+        ymax = max(y)
+        
+    if bins:
+        ax_marg_x.hist(x, density = False, color = c, alpha = alpha, bins = bins[0], 
+                       align = 'mid')
+        ax_marg_y.hist(y, density = False, color = c, alpha = alpha, bins = bins[1], 
+                       align = 'mid', orientation="horizontal")
+    else:  
+        ax_marg_x.hist(x, density = False, color = c, alpha = alpha, range = (xmin, xmax), 
+                       align = 'mid')
+        ax_marg_y.hist(y, density = False, color = c, alpha = alpha,  range = (ymin, ymax), 
+                       align = 'mid', orientation="horizontal")
+    
+    if logscale:
+        ax_joint.set_xscale('log')
+        ax_joint.set_yscale('log')
+        ax_marg_x.set_xscale('log')
+        ax_marg_x.set_yscale('log')
+        ax_marg_y.set_xscale('log')
+        ax_marg_y.set_yscale('log')
+    else:
+        if xdelta is None:
+            xdelta = (xmax - xmin)/100.
+        if ydelta is None:
+            ydelta = (ymax - ymin)/100.
+        ax_joint.axis([xmin-xdelta, xmax+xdelta, ymin-ydelta, ymax+ydelta])
+        ax_marg_x.set_xlim([xmin-xdelta, xmax+xdelta])
+        ax_marg_y.set_ylim([ymin-ydelta, ymax+ydelta])
+
+    # Turn off tick labels on marginals
+    plt.setp(ax_marg_x.get_xticklabels(), visible=False)
+    plt.setp(ax_marg_y.get_yticklabels(), visible=False)
+
+    # Set labels on joint
+    if joint_xlabel is None:
+        try:
+            joint_xlabel = x.name
+        except:
+            joint_xlabel = ''
+    if joint_ylabel is None:
+        try:
+            joint_ylabel = y.name
+        except:
+            joint_ylabel = ''
+        
+    ax_joint.set_xlabel(joint_xlabel)
+    ax_joint.set_ylabel(joint_ylabel)
+
+    # Set labels on marginals
+    if marginal_xlabel is None:
+        marginal_xlabel = 'Count'
+    if marginal_ylabel is None:
+        marginal_ylabel = 'Count'
+        
+    ax_marg_y.set_xlabel(marginal_xlabel)
+    ax_marg_x.set_ylabel(marginal_ylabel )
+    
+    if highlighting is not None:
+        for lb, rb, c, label in highlighting:
+            ax_joint.axvspan(lb, rb, alpha=0.25, color=c, label = label)
+
+    if edge_values is not None:
+        for v, c, label in edge_values:
+            ax_joint.axvline(x=v, color=c, linewidth = 3., 
+                    linestyle = '--',  label = label)
+
+    if anchor_legend is not None:
+        ax_joint.legend(bbox_to_anchor= anchor_legend)
+
+
+    return fig, ax_joint, ax_marg_x, ax_marg_y
